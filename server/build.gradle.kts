44c4a82a9da1087cc74085154d25d92679594437
Anton <a.d.ablov@gmail.com>
2023-01-31 18:45:11 +0100

    Create simple websocket chat as an initial example

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,16 +1,27 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("io.ktor.plugin") version "2.2.2"
     application
 }
 
 group = "me.anton.ablov"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation(kotlin("test"))
 }
 
